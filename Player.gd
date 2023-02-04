1716b36ef29a4a28300e73d5c93e70778744db54
AwesomlyC <victorchhun55@gmail.com>
2023-02-04 12:49:52 -0800

    Add files via upload
    Updated Health System

--- Player.gd
+++ Player.gd
@@ -1,48 +1,96 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-# Size of the window/Scene
-var screen_size;
-# Export allows this to become a "Script Variable."
-# This allows us to be able to modify the speed in the 
-# "Inspector" tab.
-# This is in pixel/seconds
-export var player_speed = 400;
-var movement = Vector2();
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	# Screen_size will be set
-	#hide();
-	pass
-	#pass # Replace with function body.
+signal grounded_updated(is_grounded)
+signal health_updated(health)
+signal killed()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-# This function will process the player's movement
-func _process(delta):
-	movement = Vector2();
-	# When the user inputs a movement key
-	# Right --> Positive X
-	# Left 	--> Negative X
-	# Up 	-->	Positive Y
-	# Down	--> Negative Y
-	if Input.is_action_pressed("move_right"):
-		$AnimatedSprite.animation = "Main";
-		movement.x += 1;	
+var score : int = 0  # +1 for each Petr
+var speed : int = 200  # speed of player
+var jumpForce : int = 400  # force of jumps
+var gravity : int = 1000  # pulls player down
+var max_health : int = 100
+
+var vel : Vector2 = Vector2()
+
+# sprite variable references sprite node
+# var sprite : Sprite = $Sprite  
+
+onready var player : Sprite = get_node("Sprite")
+onready var health = max_health setget _set_health
+onready var invulnerability_timer = $Invulnerability_timer
+
+func _physics_process(delta):
+	# gets called 60 times a second
+	vel.x = 0
+	
+	# move inputs
 	if Input.is_action_pressed("move_left"):
-		$AnimatedSprite.animation = "Movement";
-		movement.x -= 1;
+		vel.x -= speed
+	if Input.is_action_pressed("move_right"):
+		vel.x += speed
+	
+	# jump input
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		vel.y -= jumpForce
+	
+	# apply velocity	
+	vel = move_and_slide(vel, Vector2.UP)
+	
+	# gravity
+	vel.y += gravity * delta
+	
+	# player direction
+	if vel.x < 0:
+		player.flip_h = true
+	elif vel.x > 0:
+		player.flip_h = false
 
-	# Converts these function calls to jump
-	if Input.is_action_pressed("move_up"):
-		movement.y -= 1;
-	if Input.is_action_pressed("move_down"):
-		movement.y += 1;
-	# Normalize the speed so bi-directional key presses are not faster 
-	# than simply pressing one key
-	movement = movement.normalized() * player_speed;
-	move_and_slide(movement);
+func hurt():
+	_set_health(health - 10);
+	#health -= 10
+	print(health)
+func kill():
+	print("PLAYER HAS DIED!!!");
+
+func _set_health(value):
+	# Temporary Variable to remember the health value
+	var prev_health = health
+	health = max(0, value)
+	# Same as => health = clamp(value, 0, max_health);
+	# Indication that the player has taken some form of damage
+	if prev_health != health:
+		emit_signal("health_updated", health);
+		if (health == 0):
+			kill();
+			emit_signal("killed");
+	
+#func get_health() -> int:
+#	return health
+
+func _on_Player_health_updated():
+	# Temporary Variable to remember the health value
+	
+	if (invulnerability_timer.get_time_left() == 0.0 and invulnerability_timer.is_stopped()):
+		print("TIMER OUT")
+	if (invulnerability_timer.is_stopped()):
+		invulnerability_timer.start()
+		modulate.a = 0.5
+	#	for i in 2:
+			
+			#modulate.a = 0.5;
+			
+			#modulate.a = 1;
+		
+		print(">>>>>>>>>>>>>>");
+		var prev_health = health
+		health = max(0, health - 10)
+		# Same as => health = clamp(value, 0, max_health);
+		# Indication that the player has taken some form of damage
+		if prev_health != health:
+			#emit_signal("health_updated");
+			
+			print(str(health) + " <<<<<<<<<<<<<<<<<<<<<");
+			if (health == 0):
+				kill();
+				emit_signal("killed");
