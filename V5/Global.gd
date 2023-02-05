2d5aaf2a8850772f8424476eb2036e2b8783b927
AwesomlyC <victorchhun55@gmail.com>
2023-02-04 19:42:12 -0800

    Add files via upload

--- V5/Global.gd
+++ V5/Global.gd
@@ -1,5 +1,9 @@
 extends Node
 
+# Global Variables
+# max_health 	--> For Anteater
+# time 			--> Time amount per play
+# from_level	--> For level
 var max_health = 30;
 var time = 30;
 var from_level;
